91dff525e7106dbd4f1e3bedbfa2d625063d7295
Kirillov Alexander <kirillovbrest@gmail.com>
2021-06-15 14:42:47 +0300

    Names of files fixed

--- 1.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE PERSONS (
-  name varchar(30) NOT NULL,
-  surname varchar(50) NOT NULL,
-  age int NOT NULL,
-  phone_number varchar(25),
-  city_of_living varchar(25),
-  CONSTRAINT PK_Persons PRIMARY KEY (name, surname, age)
-);
-
